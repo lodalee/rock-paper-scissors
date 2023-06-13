9a3da43b9df9f0902df397336bb1667b8fc9c18a
chaewon <cwlee4633@naver.com>
2023-06-13 14:44:03 +0900

    first 구구단 commit

---
+++ src/week02/base/W10.java
@@ -0,0 +1,19 @@
+package week02.base;
+
+import java.util.Scanner;
+
+public class W10 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 9 ; i++) {
+            for (int j = 1; j <= 9; j++){
+                System.out.println(i + " X " + j + " = " + (i*j));
+            }
+        }
+    }
+}
+
+// for 문
+
+// for 문
+
+// 향상된 for 문
